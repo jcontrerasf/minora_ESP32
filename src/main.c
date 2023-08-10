1dd8eefab4f5530ca5ec903a37d2cd9940cc52be
Julio Contreras <julio.contrerasf@sansano.usm.cl>
2023-08-09 23:45:42 -0300

    Primer commit

---
+++ src/main.c
@@ -0,0 +1,358 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/services/bas.h>
+#include <zephyr/bluetooth/services/hrs.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+
+#define WIFI_SSID "CoFu5"
+#define WIFI_PASS "surfboard1234"
+
+// #define MAX_BUF_LEN 64
+// #define STACK_SIZE 1024
+
+
+static K_SEM_DEFINE(wifi_connected, 0, 1);
+static K_SEM_DEFINE(ipv4_address_obtained, 0, 1);
+
+static struct net_mgmt_event_callback wifi_cb;
+static struct net_mgmt_event_callback ipv4_cb;
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+
+    if (status->status)
+    {
+        printk("Connection request failed (%d)\n", status->status);
+    }
+    else
+    {
+        printk("Connected\n");
+        k_sem_give(&wifi_connected);
+    }
+}
+
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
+{
+    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+
+    if (status->status)
+    {
+        printk("Disconnection request (%d)\n", status->status);
+    }
+    else
+    {
+        printk("Disconnected\n");
+        k_sem_take(&wifi_connected, K_NO_WAIT);
+    }
+}
+
+static void handle_ipv4_result(struct net_if *iface)
+{
+    int i = 0;
+
+    for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+
+        char buf[NET_IPV4_ADDR_LEN];
+
+        if (iface->config.ip.ipv4->unicast[i].addr_type != NET_ADDR_DHCP) {
+            continue;
+        }
+
+        printk("IPv4 address: %s\n",
+                net_addr_ntop(AF_INET,
+                                &iface->config.ip.ipv4->unicast[i].address.in_addr,
+                                buf, sizeof(buf)));
+        printk("Subnet: %s\n",
+                net_addr_ntop(AF_INET,
+                                &iface->config.ip.ipv4->netmask,
+                                buf, sizeof(buf)));
+        printk("Router: %s\n",
+                net_addr_ntop(AF_INET,
+                                &iface->config.ip.ipv4->gw,
+                                buf, sizeof(buf)));
+        }
+
+        k_sem_give(&ipv4_address_obtained);
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
+{
+    switch (mgmt_event)
+    {
+
+        case NET_EVENT_WIFI_CONNECT_RESULT:
+            handle_wifi_connect_result(cb);
+            break;
+
+        case NET_EVENT_WIFI_DISCONNECT_RESULT:
+            handle_wifi_disconnect_result(cb);
+            break;
+
+        case NET_EVENT_IPV4_ADDR_ADD:
+            handle_ipv4_result(iface);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void wifi_connect(void)
+{
+    struct net_if *iface = net_if_get_default();
+
+    struct wifi_connect_req_params wifi_params = {0};
+
+    wifi_params.ssid = WIFI_SSID;
+    wifi_params.psk = WIFI_PASS;
+    wifi_params.ssid_length = strlen(WIFI_SSID);
+    wifi_params.psk_length = strlen(WIFI_PASS);
+    wifi_params.channel = WIFI_CHANNEL_ANY;
+    wifi_params.security = WIFI_SECURITY_TYPE_PSK;
+    wifi_params.band = WIFI_FREQ_BAND_2_4_GHZ; 
+    wifi_params.mfp = WIFI_MFP_OPTIONAL;
+
+    printk("Connecting to SSID: %s\n", wifi_params.ssid);
+
+    if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
+    {
+        printk("WiFi Connection Request Failed\n");
+    }
+}
+
+void wifi_status(void)
+{
+    struct net_if *iface = net_if_get_default();
+    
+    struct wifi_iface_status status = {0};
+
+    if (net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,	sizeof(struct wifi_iface_status)))
+    {
+        printk("WiFi Status Request Failed\n");
+    }
+
+    printk("\n");
+
+    if (status.state >= WIFI_STATE_ASSOCIATED) {
+        printk("SSID: %-32s\n", status.ssid);
+        printk("Band: %s\n", wifi_band_txt(status.band));
+        printk("Channel: %d\n", status.channel);
+        printk("Security: %s\n", wifi_security_txt(status.security));
+        printk("RSSI: %d\n", status.rssi);
+    }
+}
+
+void wifi_disconnect(void)
+{
+    struct net_if *iface = net_if_get_default();
+
+    if (net_mgmt(NET_REQUEST_WIFI_DISCONNECT, iface, NULL, 0))
+    {
+        printk("WiFi Disconnection Request Failed\n");
+    }
+}
+
+#define BT
+#ifdef BT
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+		      BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
+		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
+		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
+};
+
+typedef struct {
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hours;
+	uint8_t minutes;
+	uint8_t seconds;
+	uint8_t day_of_week;
+	uint8_t exact_time_256;
+	uint8_t adjust_reason;
+} cts_datetime_t;
+
+//cts_datetime_t datetime;
+
+static struct {
+	int offset;
+	cts_datetime_t datetime;
+} m_read_buf;
+
+static struct bt_uuid_16 uuid = BT_UUID_INIT_16(0);
+static struct bt_gatt_read_params read_params;
+
+uint8_t cts_sync_read(struct bt_conn *conn, uint8_t err,
+				    struct bt_gatt_read_params *params,
+				    //const void *data, u16_t length)
+				    const void *data, uint16_t length)
+{
+
+	if (!data || length <= 0) {
+		//sync_cts_to_clock(&m_read_buf.datetime);
+		return BT_GATT_ITER_STOP;
+	}
+
+	memcpy(&m_read_buf.datetime + m_read_buf.offset, data, length);
+	m_read_buf.offset += length;
+  printk("%d:%d:%d\n", m_read_buf.datetime.hours, m_read_buf.datetime.minutes, m_read_buf.datetime.seconds);
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err 0x%02x)\n", err);
+	} else {
+		printk("Connected\n");
+		memcpy(&uuid, BT_UUID_CTS_CURRENT_TIME, sizeof(uuid));
+		memset(&read_params, 0, sizeof(read_params));
+		read_params.func = cts_sync_read;
+		read_params.by_uuid.uuid = (struct bt_uuid *) &uuid;
+		read_params.by_uuid.start_handle = 0x0001;//attr->handle;
+		read_params.by_uuid.end_handle = 0xffff;
+		m_read_buf.offset = 0;
+		bt_gatt_read(conn, &read_params);
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void bt_ready(void)
+{
+	int err;
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static struct bt_conn_auth_cb auth_cb_display = {
+	.cancel = auth_cancel,
+};
+
+static void bas_notify(void)
+{
+	uint8_t battery_level = bt_bas_get_battery_level();
+
+	battery_level--;
+
+	if (!battery_level) {
+		battery_level = 100U;
+	}
+
+	bt_bas_set_battery_level(battery_level);
+}
+
+static void hrs_notify(void)
+{
+	static uint8_t heartrate = 90U;
+
+	/* Heartrate measurements simulation */
+	heartrate++;
+	if (heartrate == 160U) {
+		heartrate = 90U;
+	}
+
+	bt_hrs_notify(heartrate);
+}
+
+#endif
+
+int main(void)
+{
+	int err;
+
+	printk("Iniciando app\n");
+
+#ifdef BT
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	bt_ready();
+
+	bt_conn_auth_cb_register(&auth_cb_display);
+#endif
+
+	net_mgmt_init_event_callback(&wifi_cb, wifi_mgmt_event_handler,
+                                 NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT);
+
+    net_mgmt_init_event_callback(&ipv4_cb, wifi_mgmt_event_handler, NET_EVENT_IPV4_ADDR_ADD);
+
+    net_mgmt_add_event_callback(&wifi_cb);
+    net_mgmt_add_event_callback(&ipv4_cb);
+
+
+    wifi_connect();
+    k_sem_take(&wifi_connected, K_FOREVER);
+    wifi_status();
+    k_sem_take(&ipv4_address_obtained, K_FOREVER);
+    printk("Ready...\n\n");
+
+	while (1) {
+		k_sleep(K_SECONDS(1));
+        printk("lalala\n");
+
+#ifdef BT
+		/* Heartrate measurements simulation */
+		hrs_notify();
+
+		// /* Battery level simulation */
+		bas_notify();
+#endif
+	}
+	return 0;
+}
